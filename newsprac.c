906ba25eba10f35a588352ce9ff62196edcd9e5a
MAGUMBA INNOCENT <browninnocent98@gmail.com>
2018-08-18 17:04:40 +0300

    c program for innocentmagumba.github.io
    c program for innocentmagumba.github.io

---
+++ newsprac.c
@@ -0,0 +1,76 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main()
+{
+	printf("hello brothers\n\t");
+	printf("hello brothers\n\t");
+	printf("hello brothers\n\t");printf("hello brothers\n\t");
+	printf("hello brothers\n\t");
+	printf("hello brothers\n\t");
+	printf("hello brothers\n\t");printf("bree\n\t");
+	printf("345 is %f %d\n\t", 56.0,67);
+	printf("hello %lf brothers\n\t", 34678.876345678924);
+	printf("hello %c brothers\n\t", 'y');
+	printf("hello %x brothers\n\t", 16);
+	puts("hello brothers\n\t");
+	printf("\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb");
+	long cr;
+	D:
+	printf("put no (max_ten digits):\t ");
+	scanf("%ld", &cr);
+	printf("\n\thello brothers\n\t");
+	printf("hello brothers\n");
+	long fre;
+	long tad;
+	float pad;
+	fre = cr + 35*2;
+	printf("\t%ld", fre);
+	tad=fre*12;
+	printf("\n\tDear , your tad is:\t %ld", tad);
+	pad = tad/fre;
+	int f=(tad%fre);
+	printf("\n\n\tpad-figure,	%lf with modulus: %d", pad,f);
+	float g=(5/2);
+	int r=5%2;
+	printf("\n\n\t%f, %d", g,r);
+	printf("\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb\xdb");
+	 
+	 int yam, cat;
+	 cat = pad*3;
+	 yam = (fre>1234) ? cat : cr;
+	 printf("\n\n\tthe yam-no. is: %d\n\t", yam);
+	 
+	 
+	 int e                         ;
+	 puts("put 1 to continue or any other to exit");
+	 scanf("%d", &e);
+	 if(e==1){
+	 goto D;	
+	 }
+	 else;
+	 
+	 
+	 
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return (tad);
+}
